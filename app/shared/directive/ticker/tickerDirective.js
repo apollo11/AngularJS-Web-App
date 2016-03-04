d4f041e2ebf538d8c5e028e50b1b6ebc6e7eaac1
apollomm <amalapote@gmail.com>
2016-03-04 20:08:43 +0800

    Added file directive ticker

---
+++ app/shared/directive/ticker/tickerDirective.js
@@ -0,0 +1,29 @@
+app.directive('ticker',function() {
+    return {
+        restrict: 'AE',
+        scope: {
+            code: '@'
+        },
+        controller: function ( Ticker, $scope) {
+
+            $scope.params = {
+                info: 1,
+                casino: 'playtech',
+                game: $scope.code,
+                currency: 'CNY'
+            };
+            $scope.ticker = Ticker.getGamePrice($scope.params, function(resp) {
+
+                $scope.gameData = {};
+                if(!_.isUndefined(resp.request)){
+                    $scope.gameData.response =resp.request.gamedata;
+                    }
+            },
+            function (err) {
+                    console.log(err)
+                })
+        },
+        template: '<p class="jackpot" ng-repeat ="(key, value) in gameData.response"  ng-if="$index < 1">' +
+        '{{value.amount._sign}} {{value.amount.__text}}' + '</p>'
+    }
+});
---
+++ app/shared/directive/ticker/tickerService.js
@@ -0,0 +1,20 @@
+'use strict';
+
+app.factory('Ticker', ['$resource', function($resource) {
+    var endPoint =  $resource('http://tickers.playtech.com/jackpots/new_jackpotxml.php:one/:PT/:gameCode/:curr',
+        {},
+        {
+            getGamePrice: {
+                method: 'GET',
+                transformResponse: function(data, headerFn)
+                {
+                    var x2js = new X2JS();
+                    var jasonData = x2js.xml_str2json(data);
+                    return jasonData;
+
+                }
+            }
+        });
+
+    return endPoint;
+}]);
