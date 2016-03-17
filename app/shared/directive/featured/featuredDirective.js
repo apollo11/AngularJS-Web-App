b11dd935788b44c91278260b58b470fdfa88cb63
apollomm <amalapote@gmail.com>
2016-03-17 18:16:29 +0800

    Add link in featured banner

--- app/shared/directive/featured/featuredDirective.js
+++ app/shared/directive/featured/featuredDirective.js
@@ -6,7 +6,7 @@ app.directive('featuredBanner', [ function() {
         scope: {
             type: '@'
         },
-        controller: ['Menu', '$scope','imageUrl', function (Menu, $scope, imageUrl) {
+        controller: ['Menu', '$scope','imageUrl','$window', function (Menu, $scope, imageUrl, $window) {
             $scope.title = 'Test';
             $scope.imageUrl = imageUrl;
             $scope.getMenu = Menu.query();
@@ -14,10 +14,17 @@ app.directive('featuredBanner', [ function() {
                 $scope.data = {};
                 $scope.data.menu = data;
             });
+
+            $scope.onclickGameNewWindow = function (url, lang, file, title) {
+                $window.open(url+'?' + 'language='+ lang +'&game='+file, title, 'width=800, height=600');
+            }
         }],
-        template:'<div class="featured-section">'+
-                    '<div class="ftd-container" ng-repeat="(key, value ) in data.menu track by $index" ng-hide="value.path !== menuType">'+
-                        '<img class="animated fadeIn" ng-src="{{imageUrl + value.featured_banner.filename}}" />'+
+        template:'<div class="featured-section" ng-repeat="(key, value ) in data.menu track by $index" ng-hide="value.path !== menuType">'+
+                    '<a ng-click="onclickGameNewWindow(value.game_link, value.language, value.game_code)">' +
+                        '<div class="ftd-container">'+
+                                '<img class="animated fadeIn" ng-src="{{imageUrl + value.featured_banner.filename}}" />'+
+                        '</div>'+
+                        '</a>'+
                     '</div>',
         link: function (scope, elem, attrs) {
                 scope.menuType =  scope.type;
