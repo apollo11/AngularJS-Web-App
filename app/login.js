7d5f77050e03cc64c2eae959f387eb25d8926a89
apollomm <amalapote@gmail.com>
2016-03-22 14:06:54 +0800

    Add toastr

--- app/login.js
+++ app/login.js
@@ -15,22 +15,29 @@ function logout(allSessions, realMode) {
 function calloutLogin(response) {
 
     if (response.errorCode) {
-        alert("" + response.playerMessage +" Error Code  " + response.errorCode);
+        //alert("" + response.playerMessage +" Error Code  " + response.errorCode);
+        // Display a warning toast, with no title
+        toastr.error( response.playerMessage + "Error Code" + response.errorCode);
+        // Display an info toast with no title
+        toastr.info('Are you the 6 fingered man?')
     }
     else {
         var username = _.escape(document.getElementById("loginform").username.value.toUpperCase());
         document.cookie = "username="+ username;
-        alert("Login OK, you will be redirected to play console");
+        // Display a success toast, with a title
+        toastr.success('Have fun storming the castle!', 'Miracle Max Says');
+        //alert("Login OK, you will be redirected to play console");
         location.reload();
     }
 }
 
 function calloutLogout(response) {
     if (response.errorCode) {
-        alert("Logout failed, " + response.errorCode);
+        toastr.error("Logout failed, " + response.errorCode);
     }
     else {
         delete_cookie();
+        toastr.success('You are now logged out!');
         location.reload();
     }
 }
