d529e1ab6ae4dda4490e6d1fab62a5a80c6cce4f
apollomm <amalapote@gmail.com>
2016-03-22 14:39:50 +0800

    Add close button in toaster

--- app/login.js
+++ app/login.js
@@ -13,23 +13,21 @@ function logout(allSessions, realMode) {
 }
 
 function calloutLogin(response) {
-
+    toastr.options.closeButton = true;
     if (response.errorCode) {
-        //alert("" + response.playerMessage +" Error Code  " + response.errorCode);
-        // Display a warning toast, with no title
         toastr.error( response.playerMessage + "Error Code" + response.errorCode);
     }
     else {
         var username = _.escape(document.getElementById("loginform").username.value.toUpperCase());
         document.cookie = "username="+ username;
-        // Display a success toast, with a title
-        toastr.success('Your are now logged in!');
-        //alert("Login OK, you will be redirected to play console");
+        toastr.success('Login successful, you will be redirected to play console!');
         location.reload();
     }
 }
 
 function calloutLogout(response) {
+    toastr.options.closeButton = true;
+
     if (response.errorCode) {
         toastr.error("Logout failed, " + response.errorCode);
     }
