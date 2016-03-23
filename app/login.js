24193b96718ba30d8454d621d8f145facfec38f7
apollomm <amalapote@gmail.com>
2016-03-23 15:48:11 +0800

    Change value of anchor when login

--- app/login.js
+++ app/login.js
@@ -23,7 +23,8 @@ function calloutLogin(response) {
         document.cookie = "username="+ username;
         location.reload();
         var usernameCookie = document.cookie;
-        getCookie(usernameCookie);
+
+        validateCookie(usernameCookie);
     }
 }
 
@@ -40,13 +41,7 @@ function calloutLogout(response) {
     }
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
+function validateCookie(cookie) {
+    if(!_.isUndefined(cookie)) return;
+        document.getElementsByClassName('login-link').text = 'LOGOUT';
 }
