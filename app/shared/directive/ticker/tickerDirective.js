d6b7e54329b85ec2d09f8e6166bfc15ef526c278
apollomm <amalapote@gmail.com>
2016-04-06 20:16:12 +0800

    Update ticker changes when searching

--- app/shared/directive/ticker/tickerDirective.js
+++ app/shared/directive/ticker/tickerDirective.js
@@ -1,38 +1,41 @@
-app.directive('ticker',function() {
+app.directive('ticker',['Ticker', function(Ticker) {
     return {
         restrict: 'AE',
         scope: {
             code: '@'
         },
-        controller: ['Ticker', '$scope', function (Ticker, $scope) {
+        template: '<p ng-repeat ="(key, value) in gameData.response | limitTo:1 track by $index"  ng-show="$index< 1 && amountTo !=\'\'">'+
+                    '￥ <span count-to="{{amountTo}}" value="{{amountFrom}}" duration="60"></span>' +
+                  '</p>',
+        link: function (scope, elem, attrs) {
 
-            $scope.params = {
+            scope.gameCode =  scope.code;
+            scope.params = {
                 info: 1,
                 casino: 'playtech',
-                game: $scope.code,
+                game: scope.gameCode,
                 currency: 'CNY'
             };
-            $scope.ticker = Ticker.getGamePrice($scope.params, function(resp) {
+            scope.ticker = Ticker.getGamePrice(scope.params, function(resp) {
 
-                $scope.gameData = {};
-                if(!_.isUndefined(resp.request)){
-                    $scope.gameData.response =resp.request.gamedata;
-                    _.forEach( $scope.gameData.response, function(value, key) {
-                       if(!_.isUndefined(value.amount)){
-                           $scope.amountTo = parseFloat(value.amount.__text);
-                           $scope.amountFrom = parseFloat(value.amount.__text) * .50;
-                           console.log($scope.amountFrom);
-                           console.log($scope.amountTo);
-                       }
-                    });
-                 }
-            },
-            function (err) {
+                    scope.gameData = {};
+                    if(!_.isUndefined(resp.request)) {
+                        scope.gameData.response = resp.request.gamedata;
+                        _.forEach( scope.gameData.response, function(value, key) {
+                            if(!_.isUndefined(value.amount)) {
+                                scope.amountTo = parseFloat(value.amount.__text);
+                                scope.amountFrom = parseFloat(value.amount.__text) * .50;
+                                console.log(scope.amountFrom);
+                                //console.log(scope.amountTo);
+                            }else {
+                                scope.amountValue = true;
+                            }
+                        });
+                    }
+                },
+                function (err) {
                     console.log(err)
                 })
-        }],
-        template: '<p class="jackpot" ng-repeat ="(key, value) in gameData.response track by $index"  ng-if="$index < 1">' +
-        '￥' + '<span count-to="{{amountTo}}" value="{{amountFrom}}" duration="60"></span>'+
-        '</p>'
+        }
     }
-});
+}]);
